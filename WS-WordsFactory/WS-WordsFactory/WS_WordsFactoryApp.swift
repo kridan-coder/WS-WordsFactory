67ad7ed66f38cc327c013da72101fa54cb90eab0
Daniel <d.krivelev@redmadrobot.com>
2022-04-15 23:22:54 +0700

    Basic setup

---
+++ WS-WordsFactory/WS-WordsFactory/WS_WordsFactoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WS_WordsFactoryApp.swift
+//  WS-WordsFactory
+//
+//  Created by Daniel Krivelev on 15.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct WS_WordsFactoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

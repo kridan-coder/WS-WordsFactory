8f646b2250530d1bb539548d0cd5fcfe571adf7b
Daniel <d.krivelev@redmadrobot.com>
2022-04-16 23:37:33 +0700

    make models for realmSwift and api requests
    you cannot complain. you cannot cry. your life is war. and you must win. or die.

--- WS-WordsFactory/WS-WordsFactory/Dictionary/Dictionary.swift
+++ WS-WordsFactory/WS-WordsFactory/Dictionary/Dictionary.swift
@@ -6,13 +6,83 @@
 //
 
 import SwiftUI
+import Alamofire
+import RealmSwift
+
+struct Response: Codable {
+  enum CodingKeys: String, CodingKey {
+    case word, phonetic, meaning = "meanings"
+  }
+  
+  let word: String
+  let phonetic: String
+  let meaning: Meaning
+  
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    word = try container.decode(String.self, forKey: .word)
+    phonetic = try container.decode(String.self, forKey: .phonetic)
+    let meanings = try container.decode([Meaning].self, forKey: .meaning)
+    meaning = meanings[0]
+  }
+}
+
+struct Meaning: Codable {
+  let partOfSpeech: String
+  let definitions: [Definition]
+}
+
+struct Definition: Codable {
+  let definition: String
+  let example: String
+}
+
+class RealmResponse: Object, Identifiable {
+  @objc dynamic var id = UUID()
+  @objc dynamic var word = ""
+  @objc dynamic var phonetic = ""
+  let meaning: Meaning? = nil
+}
+
+class RealmMeaning: Object {
+  @objc dynamic var partOfSpeech = ""
+  let definitions = RealmSwift.List<RealmDefinition>()
+}
+
+class RealmDefinition: Object {
+  @objc dynamic var definition = ""
+  @objc dynamic var example = ""
+}
+
 
 struct Dictionary: View {
   @State var currentTab = 0
+  @ObservedResults(RealmResponse.self) var responses
+  @State var searchText = ""
+  @State var searchResult: Response? = nil
+  
     var body: some View {
       VStack {
+        HStack {
+          TextField("", text: $searchText)
+          Spacer()
+          
+        }
+        
         if currentTab == 0 {
-          Text("HHEHEHEHHEHEHHEHHHEHEH")
+          if searchText == "", searchResult == nil {
+            VStack {
+              Spacer()
+              Image("CoolKidsPhone").resizable().scaledToFit().padding()
+              Text("No word")
+                .font(.custom("Rubik-Medium", size: 24))
+                .padding(.bottom, 5)
+              Text("Input something to find it in dictionary")
+                .font(.custom("Rubik-Regular", size: 16))
+                .foregroundColor(Color("InkGray"))
+                Spacer()
+            }
+          }
         } else if currentTab == 1 {
           Text("Training")
         } else if currentTab == 2 {
